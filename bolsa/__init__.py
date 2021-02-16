bc9bcd64f43251dee64800f561e939b375cb3d01
LautaroParada <lautaro.parada.opazo@gmail.com>
2021-02-16 14:50:58 -0300

    Estructura inicial del cliente de la API

---
+++ bolsa/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 16 14:39:46 2021
+
+@author: lauta
+"""
+
+from .cliente import RESTCliente
---
+++ bolsa/modelos/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 16 14:44:14 2021
+
+@author: lauta
+"""
+
